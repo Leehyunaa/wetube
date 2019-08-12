433ae88c07bb71d718deb6eb3990a78dd18f0bb0
LeeHyunAh <lha940610@naver.com>
2019-08-12 20:00:57 +0900

    add view

--- app.js
+++ app.js
@@ -7,15 +7,18 @@ import userRouter from "./routes/userRouter";
 import videoRouter from "./routes/videoRouter";
 import globalRouter from "./routes/globalRouter";
 import routes from "./routes";
+import { localMiddleware } from "./middlewares";
 
 const app = express();
 
-
+app.use(helmet());
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
+
+app.use(localMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);

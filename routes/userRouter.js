1824ec9d138cb7248101370760f34bb80978e806
LeeHyunAh <lha940610@naver.com>
2019-08-15 20:00:29 +0900

    add 2.25

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,7 +1,6 @@
 import express from "express";
 import routes from "../routes";
 import {
-    users,
     userDetail,
     editProfile,
     changePassword
@@ -10,10 +9,10 @@ import {
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, users);
-userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail(), userDetail);
+
 
 
 export default userRouter;

bc4f3320deb87479e3c4b8d1fba326fa6f957b1f
LeeHyunAh <lha940610@naver.com>
2019-08-28 18:52:05 +0900

    mongo store

--- routes/globalRouter.js
+++ routes/globalRouter.js
@@ -1,7 +1,7 @@
 import express from "express";
 import routes from "../routes";
 import { onlyPublic } from "../middlewares";
-import { search } from "../controllers/videoController";
+import { search, home } from "../controllers/videoController";
 import {
   postJoin,
   getJoin,
@@ -18,7 +18,8 @@ globalRouter.post(routes.join, onlyPublic, postJoin, postLogin);
 globalRouter.get(routes.login, onlyPublic, getLogin);
 globalRouter.post(routes.login, onlyPublic, postLogin);
 
-globalRouter.get(routes.logout, logout);
+globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
+globalRouter.get(routes.logout, onlyPublic, logout);
 
 export default globalRouter;
